d7fff32874a3d13e370995dc5ffb227423cc53ca
Tushar Koshti <tushar@zettabolt.com>
2024-12-01 23:58:38 +0530

    port changed

--- src/Renderer/ReactRendererFlow.js
+++ src/Renderer/ReactRendererFlow.js
@@ -582,7 +582,7 @@ function ReactRendererFlow() {
     };
 
     try {
-      const response = await fetch("http://localhost:5000/api/saveGraph", {
+      const response = await fetch("http://localhost:4000/api/saveGraph", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(graphData),
